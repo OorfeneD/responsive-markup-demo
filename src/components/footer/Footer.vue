c7478cc5a9fa823fc586079ec687d6b1bdf4f340
oorfened <oorfened@gmail.com>
2022-01-30 23:36:21 +0300

    feat: Footer section added with Links component.
    Uses footer-back.png from public folder as asset (FIXME)

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,101 @@
+<template>
+  <footer>
+    <div class="footer-container">
+      <div class="logo-container">
+        <div class="line"></div>
+        <Logo />
+        <div class="line"></div>
+      </div>
+      <div class="links">
+        <links
+          title="About"
+          :items="['Shop', 'Plan my kitchen', 'About us', 'Gallery']"
+        />
+        <links
+          title="Service"
+          :items="['FAQ', 'Contact', 'How to buy', 'Downloads']"
+        />
+        <links title="Info" :items="['Delivery', 'Terms', 'Privacy']" />
+        <links
+          title="Follow"
+          :socials="true"
+          :items="[
+            ['fab', 'facebook-f'],
+            ['fab', 'twitter'],
+            ['fab', 'instagram'],
+          ]"
+        />
+      </div>
+      <div class="special-info">
+        <p>Copyright Online MTC Home Kitchens 2018 - All rights reserved.</p>
+        <p>Responsive website design, Development & Hosting by mtc.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import Logo from "../shared/Logo.vue";
+import Links from "../footer/Links.vue";
+
+export default {
+  components: { Logo, Links },
+};
+</script>
+
+<style lang="scss">
+footer {
+  background-image: url("/images/footer-back.png");
+  background-position: center;
+  background-size: cover;
+  color: $white;
+  .footer-container {
+    padding: 40px 1ch 3rem 1ch;
+    border-bottom: 1px solid rgba($color: #fff, $alpha: 0.2);
+    margin-bottom: 3.5rem;
+    .logo-container {
+      display: grid;
+      grid-template-columns: 1fr auto 1fr;
+      gap: 20px;
+      .line {
+        border-bottom: 1px solid rgba($color: #fff, $alpha: 0.2);
+        margin: auto;
+        height: 0px;
+      }
+    }
+    .links {
+      display: flex;
+      justify-content: center;
+      align-items: top;
+      flex-wrap: wrap;
+      gap: 20px;
+      padding: 2rem 0 2.5rem 0;
+    }
+    .special-info {
+      text-align: center;
+      font-size: 13px;
+      line-height: 2em;
+      opacity: 0.5;
+    }
+    @media screen and (max-width: 900px) {
+      .links {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        .links-block {
+          margin: 0 auto;
+        }
+      }
+    }
+    @media screen and (max-width: 500px) {
+      .links {
+        text-align: center;
+        display: grid;
+        grid-template-columns: 1fr;
+        .links-block__items--socials {
+          justify-content: center;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/footer/Links.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="links-block">
+    <div class="links-block__title">{{ title }}</div>
+    <div v-if="socials" class="links-block__items links-block__items--socials">
+      <div v-for="(item, i) in items" :key="i" class="links-block__item">
+        <div class="links-block__item">
+          <font-awesome-icon :icon="item" />
+        </div>
+      </div>
+    </div>
+    <div v-else class="links-block__items">
+      <div v-for="(item, i) in items" :key="i" class="links-block__item">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faFacebookF, faTwitter, faInstagram);
+
+export default {
+  components: { FontAwesomeIcon },
+  props: {
+    socials: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: "Title",
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.links-block {
+  max-width: 200px;
+  &__title {
+    font-size: 24px;
+    line-height: 1.75em;
+    font-family: Helvetica, sans-serif;
+    padding-bottom: 1em;
+    font-weight: bold;
+  }
+  &__items {
+    font-size: 13px;
+    line-height: 2.3em;
+    letter-spacing: 0.35ch;
+    text-transform: uppercase;
+    &--socials {
+      display: flex;
+      gap: 1ch;
+      line-height: unset;
+      letter-spacing: unset;
+      div {
+        width: 40px;
+        height: 40px;
+        border-radius: 100%;
+        background-color: $white;
+        color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 20px;
+      }
+    }
+  }
+}
+</style>
