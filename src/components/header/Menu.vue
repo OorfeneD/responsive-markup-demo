cf9e2d0b707268d4c58d734e14839c235e8c1696
oorfened <oorfened@gmail.com>
2022-01-30 23:35:17 +0300

    fix: missing Menu component in header added

---
+++ src/components/header/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="menu">
+    <div v-if="!logo" class="menu__links">
+      <a href="#" class="menu__link">Shop</a>
+      <a href="#" class="menu__link">Plan my kitchen</a>
+      <a href="#" class="menu__link">About us</a>
+      <a href="#" class="menu__link">Gallery</a>
+    </div>
+    <div v-if="logo" class="menu__links">
+      <a href="#" class="menu__link">Shop</a>
+      <a href="#" class="menu__link">Plan my kitchen</a>
+    </div>
+    <div v-if="logo" class="menu__logo">
+      <Logo />
+    </div>
+    <div v-if="logo" class="menu__links">
+      <a href="#" class="menu__link">About us</a>
+      <a href="#" class="menu__link">Gallery</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "../shared/Logo.vue";
+export default {
+  props: {
+    logo: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  components: { Logo },
+};
+</script>
+
+<style></style>
